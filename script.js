2bca0202aa2a5546d4a0e65416294bd1fcbb2c3e
Temnova Vika <vikaten06@gmail.com>
2024-03-23 00:48:03 +0300

    создать шаблон слайдера

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 "use strict";
-const a = 9;
-console.log(a);
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const slider = document.querySelector('.slider');
+let setleft = 0;
+if (next != null) {
+    next.addEventListener('click', function () {
+        if (slider != null) {
+            setleft -= 620;
+            slider.style.left = setleft + 'px';
+            if (setleft < -1240) {
+                if (slider != null) {
+                    slider.style.left = 0 + 'px';
+                    setleft = 0;
+                }
+            }
+        }
+    });
+}
+if (prev != null) {
+    prev.addEventListener('click', function () {
+        if (slider != null) {
+            setleft += 620;
+            slider.style.left = setleft + 'px';
+            if (setleft > 0) {
+                if (slider != null) {
+                    slider.style.left = -1240 + 'px';
+                    setleft = -1240;
+                }
+            }
+        }
+    });
+}
--- script.ts
+++ script.ts
@@ -1,2 +1,33 @@
-const a: number = 9;
-console.log(a);
+const next: HTMLButtonElement | null = document.querySelector('#next');
+const prev: HTMLButtonElement | null = document.querySelector('#prev');
+const slider: HTMLElement | null = document.querySelector('.slider');
+let setleft: number = 0;
+if (next != null) {
+    next.addEventListener('click', function() {      
+        if (slider != null) {
+            setleft -= 620;
+            slider.style.left = setleft + 'px';
+            if (setleft < -1240) {
+                if (slider != null) {
+                    slider.style.left = 0 + 'px';
+                    setleft = 0;
+                }
+            }
+        }
+    })
+}
+
+if (prev != null) {
+    prev.addEventListener('click', function() {      
+        if (slider != null) {
+            setleft += 620;
+            slider.style.left = setleft + 'px';
+            if (setleft > 0) {
+                if (slider != null) {
+                    slider.style.left = -1240 + 'px';
+                    setleft = -1240;
+                }
+            }
+        }
+    })
+}
