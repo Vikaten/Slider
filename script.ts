ee3eee19c2fdaf6c2ad603e47a812266890d8fec
Temnova Vika <vikaten06@gmail.com>
2024-03-23 14:29:35 +0300

    попытаться создать фишку рандомной смены картинок

--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
 const next: HTMLButtonElement | null = document.querySelector('#next');
 const prev: HTMLButtonElement | null = document.querySelector('#prev');
 const slider: HTMLElement | null = document.querySelector('.slider');
+const img: NodeListOf<Element> = document.querySelectorAll('.img');
 let setleft: number = 0;
 if (next != null) {
     next.addEventListener('click', function() {      
@@ -31,3 +32,15 @@ if (prev != null) {
         }
     })
 }
+
+const numbersArray = Array.from({ length: img.length }, (_, index) => index);
+console.log(numbersArray);
+
+setInterval(() => {
+    for (let i = 0; i < numbersArray.length; i++) {
+        let rel = (img[i] as HTMLImageElement).src;
+        let randomIndex = Math.floor(Math.random() * numbersArray.length);
+        (img[i] as HTMLImageElement).src = (img[randomIndex] as HTMLImageElement).src;
+        (img[i] as HTMLImageElement).src = rel;
+    }
+}, 3000)
