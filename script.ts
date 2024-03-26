e308106573ba96745e79b4618ac40c04e2645b14
Temnova Vika <vikaten06@gmail.com>
2024-03-26 23:41:52 +0300

    стилизовать страницу, изменить картинки

--- script.ts
+++ script.ts
@@ -1,43 +1,43 @@
 const next: HTMLButtonElement | null = document.querySelector('#next');
 const prev: HTMLButtonElement | null = document.querySelector('#prev');
-const slider: HTMLElement | null = document.querySelector('.slider');
+const sliderLine: HTMLElement | null = document.querySelector('#slider-line');
 const img: NodeListOf<Element> = document.querySelectorAll('.img');
-let setleft: number = 0;
+let setright: number = 0;
 if (next != null) {
-    next.addEventListener('click', function() {      
-        if (slider != null) {
-            setleft -= 620;
-            slider.style.left = setleft + 'px';
-            if (setleft < -1240) {
-                if (slider != null) {
-                    slider.style.left = 0 + 'px';
-                    setleft = 0;
+    next.addEventListener('click', function () {
+        if (sliderLine != null) {
+            setright -= 540;
+            sliderLine.style.right = setright + 'px';
+            if (setright < 0) {
+                if (sliderLine != null) {
+                    sliderLine.style.right = 2720 + 'px';
+                    setright = 2720;
                 }
             }
         }
-    })
+    });
 }
 
 if (prev != null) {
-    prev.addEventListener('click', function() {      
-        if (slider != null) {
-            setleft += 620;
-            slider.style.left = setleft + 'px';
-            if (setleft > 0) {
-                if (slider != null) {
-                    slider.style.left = -1240 + 'px';
-                    setleft = -1240;
+    prev.addEventListener('click', function () {
+        if (sliderLine != null) {
+            setright += 540;
+            sliderLine.style.right = setright + 'px';
+            if (setright > 2720) {
+                if (sliderLine != null) {
+                    sliderLine.style.right = 0 + 'px';
+                    setright = 0;
                 }
             }
         }
-    })
+    });
 }
 
 const imgArray = Array.from(img);
 const numbersArray = Array.from({ length: imgArray.length }, (_, index) => index); 
 
 setInterval(() => {
-    for (let i = 0; i < imgArray.length - 1; i++) {
+    for (let i = 0; i < imgArray.length; i++) {
         let tmp = imgArray[i];
         let rnd = Math.floor(Math.random() * (i + 1));
         imgArray[i] = imgArray[rnd];
